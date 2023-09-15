980847bc7c35db0c0a4599a14b7f677c943ea720
HugoAlejandro2002 <apazahuaychohugoalejandro@gmail.com>
2023-09-15 03:07:54 -0400

    added music to game and fix collition with trash

--- main.cpp
+++ main.cpp
@@ -1,18 +1,14 @@
 #include <bits/stdc++.h>
-#include <math.h>
-#include <array>
-
 #include "picosystem.hpp"
+#include "utility/music.hpp"
 
 using namespace picosystem;
 
-// Estados del Juego
 enum state_t
 {
   PLAYING,
   GAME_OVER,
-  MENU,
-  SETTINGS
+  MENU
 };
 state_t state = MENU;
 
@@ -21,45 +17,22 @@ struct vec_t
   int32_t x, y;
 };
 
-// Fronteras y escala del mapa
 constexpr vec_t bounds{.x = 18, .y = 16};
 constexpr int scale = 6;
 
-// Selector de Juego
 bool play_selected = true;
-// Verificar si el juego está listo
 bool is_game_set = false;
+bool playerHitObstacle = false;
 
-// Selectores de Dificultad en Menu
-bool easy_selected = true;
-bool medium_selected = false;
-bool hard_selected = false;
-// Velocidad de aparición de enemigos
 int32_t difficulty = 300;
 
-// Arreglo de Sprites
-std::array<int, 6> fruit_sprites = {{
-    APPLE,
-    CHERRY,
-    BANANA,
-    ORANGE,
-    EGGPLANT,
-    STRAWBERRY,
-}};
-
-// Estructura de Jugador
 struct
 {
   vec_t dir;
   vec_t pos;
   int32_t score;
   int32_t lives;
-
-  vec_t next()
-  {
-    return {.x = pos.x + dir.x, .y = pos.y + dir.y};
-  }
-
+  vec_t next() { return {.x = pos.x + dir.x, .y = pos.y + dir.y}; }
   void move()
   {
     double L = sqrt(dir.x * dir.x + dir.y * dir.y);
@@ -69,138 +42,49 @@ struct
   }
 } player;
 
-class Fruit
+class RoadItem
 {
 public:
   vec_t pos;
   vec_t dir;
-  Fruit(int x, int y)
-  {
-    pos = {.x = x, .y = y};
-    dir = {.x = -1, .y = 0};
-  }
-  vec_t next()
-  {
-    return {.x = pos.x + dir.x, .y = pos.y + dir.y};
-  }
-
-  void move()
-  {
-    pos = next();
-  }
+  RoadItem(int x, int y) : pos{.x = x, .y = y}, dir{.x = -1, .y = 0} {}
+  vec_t next() { return {.x = pos.x + dir.x, .y = pos.y + dir.y}; }
+  void move() { pos = next(); }
 };
 
-class Bullet
-{
-public:
-  vec_t pos;
-  vec_t dir;
-  Bullet(int x, int y)
-  {
-    pos = {.x = x, .y = y};
-    dir = {.x = 1, .y = 0};
-  }
-  vec_t next()
-  {
-    return {.x = pos.x + dir.x, .y = pos.y + dir.y};
-  }
+std::deque<RoadItem> roadItems;
+std::deque<RoadItem> obstacles;
 
-  void move()
-  {
-    pos = next();
-  }
-};
-
-/*
-Funciones de la mecánica de Juego
-*/
-
-// Sonidos de selector y aceptar
 voice_t blip, enter;
-// Fruta aleatoria del arreglo fruit_sprites
-int fruit_selected;
 
-// Deques de los elementos del juego
-std::deque<Fruit> fruits;
-std::deque<Fruit> rotten_fruits;
+RoadItem place_road_item() { return RoadItem(bounds.x - 1, std::rand() % bounds.y); }
 
-Fruit place_fruit()
-{
-  return Fruit(bounds.x - 1, std::rand() % bounds.y);
-}
+void change_state(state_t _state) { state = _state; }
 
-void change_state(state_t _state)
-{
-  state = _state;
-}
+vec_t transform(vec_t v) { return {.x = (v.x * scale) + 6, .y = (v.y * scale) + 18}; }
 
-// Transformar posiciones del mapa de acuerdo a la escala
-vec_t transform(vec_t v)
-{
-  return {.x = (v.x * scale) + 6, .y = (v.y * scale) + 18};
-}
+bool in_bounds_bottom(vec_t next) { return next.y <= bounds.y - 1; }
+bool in_bounds_top(vec_t next) { return next.y >= 1; }
+bool wall_hit_check(vec_t next) { return next.x < 0 || next.x >= bounds.x || next.y < 1 || next.y >= bounds.y; }
 
-/*
-Revisión de colisión en paredes
-*/
-
-bool in_bounds_bottom(vec_t next)
-{
-  return next.y <= bounds.y - 1;
-}
-
-bool in_bounds_top(vec_t next)
-{
-  return next.y >= 1;
-}
-
-bool wall_hit_check_x(vec_t next)
-{
-  return next.x < 1 || next.x >= bounds.x - 3;
-}
-
-bool wall_hit_check_y(vec_t next)
-{
-  return next.y < 3 || next.y >= bounds.y - 3;
-}
-
-bool wall_hit_check(vec_t next)
-{
-  return next.x < 0 || next.x >= bounds.x || next.y < 1 || next.y >= bounds.y;
-}
-
-// Armar las condiciones de juego
 void set_game(int32_t _lives)
 {
-
-  // state = PLAYING;
-
   player.lives = _lives;
-
   player.pos = {.x = 0, .y = 15};
   player.dir = {.x = 1, .y = 0};
-
-  fruits.push_back(place_fruit());
-
-  fruit_selected = fruit_sprites[std::rand() % 5];
-
+  roadItems.push_back(place_road_item());
   is_game_set = true;
 }
 
 void init()
 {
-
   play_selected = true;
-  easy_selected = true;
-  medium_selected = false;
-  hard_selected = false;
   blip = voice(50, 50, 50, 50, 10, 2);
   enter = voice(100, 50, 100, 50, 20, 2);
-
   if (state == PLAYING)
   {
-    fruits.clear();
-    rotten_fruits.clear();
+    roadItems.clear();
+    obstacles.clear();
     player.score = 0;
     set_game(7);
   }
@@ -218,37 +102,38 @@ void update(uint32_t tick)
       play(enter, 2500, 50, 100);
       sleep(200);
     }
-  } else if (state == PLAYING)
+  }
+  else if (state == PLAYING)
   {
 
     if (is_game_set)
     {
 
-      // play_song(tick);
+      play_song(tick);
 
-      // Colocar frutas y enemigos
+      // Colocar obstaculos y enemigos
 
       if (tick % difficulty == 0)
       {
-        rotten_fruits.push_back(place_fruit());
+        obstacles.push_back(place_road_item());
       }
 
       if (tick % (difficulty * 2) == 0)
       {
-        fruits.push_back(place_fruit());
+        roadItems.push_back(place_road_item());
       }
 
       if (tick % 30 == 0)
       {
         if (state != GAME_OVER)
         {
-          for (auto &fruit : fruits)
+          for (auto &roadItem : roadItems)
           {
-            fruit.move();
+            roadItem.move();
           }
-          for (auto &fruit : rotten_fruits)
+          for (auto &obstacle : obstacles)
           {
-            fruit.move();
+            obstacle.move();
           }
         }
       }
@@ -277,45 +162,49 @@ void update(uint32_t tick)
           change_state(MENU);
         }
 
-        // Revisar colisiones de frutas con el jugador
-        for (auto &fruit : fruits)
+        // Revisar colisiones de obstaculos con el jugador
+        for (auto &roadItem : roadItems)
         {
-          if (intersects(next.x, next.y, sprite_size, sprite_size, fruit.next().x, fruit.next().y, sprite_size, sprite_size))
+          if (intersects(next.x, next.y, sprite_size, sprite_size, roadItem.next().x, roadItem.next().y, sprite_size, sprite_size))
           {
             player.lives = std::min(player.lives + 3, 7l); // Aumentar la vida en 3 puntos hasta un máximo de 7
             play(blip, 1000, 30, 100);
-            fruit = Fruit(std::rand() % bounds.x, 100);
+            roadItem = RoadItem(std::rand() % bounds.x, 100);
           }
-          if (wall_hit_check(fruit.pos) || fruit.pos.y >= 18)
+          if (wall_hit_check(roadItem.pos) || roadItem.pos.y >= 18)
           {
 
-            fruits.pop_front();
+            roadItems.pop_front();
           }
         }
-        
-        
+
         // Revisar si un enemigo pasó la barrera inferior
-        for (auto &rotten_fruit : rotten_fruits)
+        for (auto &obstacle : obstacles)
         {
-          if (intersects(next.x, next.y, sprite_size, sprite_size, rotten_fruit.next().x, rotten_fruit.next().y, sprite_size, sprite_size))
+          playerHitObstacle = false; // Restablecer la variable antes de cada comprobación
+          if (intersects(next.x, next.y, sprite_size, sprite_size, obstacle.next().x, obstacle.next().y, sprite_size, sprite_size))
           {
             player.lives -= 2; // Disminuir la vida en 2 puntos
             play(blip, 500, 30, 100);
-            rotten_fruit = Fruit(std::rand() % bounds.x, 100);
+            obstacle = RoadItem(std::rand() % bounds.x, 100);
+            playerHitObstacle = true; // Establecer la variable a verdadero si el jugador choca
           }
 
-          if (wall_hit_check(rotten_fruit.pos) || rotten_fruit.pos.y >= 100)
+          if (wall_hit_check(obstacle.pos) || obstacle.pos.y >= 100)
           {
-            rotten_fruits.pop_front();
+            obstacles.pop_front();
             if (player.lives <= 0)
             {
               state = GAME_OVER;
             }
             else
             {
-              player.lives -= 1;
-              player.score += 1; // Aumentar el puntaje si la fruta malévola llega al límite
-              play(blip, 1000, 30, 100);
+              if (!playerHitObstacle) // Solo descontar puntos si el jugador no chocó
+              {
+                player.lives -= 1;
+                player.score += 1; // Aumentar el puntaje si la obstáculo malévola llega al límite
+                play(blip, 1000, 30, 100);
+              }
             }
           }
         }
@@ -439,7 +328,7 @@ void draw(uint32_t tick)
 
     split_text("Car Runner \\spr150", c1, c2, 27.5, 30);
     glitch_text("Play", (255, 255, 255), menu_options_pos_x, menu_options_pos_main_y);
-    glow_text("Made by Jose and Hugo",(13,25,252), 3, 90);
+    glow_text("Made by Jose and Hugo", (13, 25, 252), 3, 90);
 
     if (play_selected)
     {
@@ -448,7 +337,9 @@ void draw(uint32_t tick)
       pen((255, 255, 255));
       frect(menu_options_pos_x - 10, menu_options_pos_main_y + 1, 5, 5);
     }
-  }else{
+  }
+  else
+  {
 
     // Background
     pen(20, 20, 54);
@@ -456,7 +347,7 @@ void draw(uint32_t tick)
 
     pen(255, 255, 255);
 
-    pen(255,255,0);
+    pen(255, 255, 0);
     frect(5, 72.5, 30, 5);
     frect(45, 72.5, 30, 5);
     frect(85, 72.5, 30, 5);
@@ -475,15 +366,15 @@ void draw(uint32_t tick)
     rect(2, 27, 116, 90);
     rect(2, 29, 116, 90);
 
-    // Dibujo de frutas
-    for (auto fruit : fruits)
+    // Dibujo de obstaculos
+    for (auto roadItem : roadItems)
     {
-      sprite(POTION_RED, transform(fruit.pos).x + 3, transform(fruit.pos).y + 3);
+      sprite(POTION_RED, transform(roadItem.pos).x + 3, transform(roadItem.pos).y + 3);
     }
     // Dibujo de enemigos
-    for (auto fruit : rotten_fruits)
+    for (auto obstacle : obstacles)
     {
-      sprite(BLOCK_STACK_STONE, transform(fruit.pos).x + 3, transform(fruit.pos).y + 3);
+      sprite(BLOCK_STACK_STONE, transform(obstacle.pos).x + 3, transform(obstacle.pos).y + 3);
     }
 
     // Dibujo de jugador (Parpadeo si hay GAME_OVER)
